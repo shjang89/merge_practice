3227e12b4b25b45429d2464be46baa805b5d5a1b
minwoo <minwoo@gmail.com>
2024-11-19 08:41:40 +0900

    minwoo

--- django-project/curd/settings.py
+++ django-project/curd/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+# 11월 19월 8시 41분 민우
+
 
 from pathlib import Path
 
