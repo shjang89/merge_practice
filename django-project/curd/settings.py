63075648adb484f2fddd3dbfa967dd8454c4fc0d
minwoo <minwoo@gmail.com>
2024-11-19 08:38:26 +0900

    minwoo

--- django-project/curd/settings.py
+++ django-project/curd/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-# 11월 19일 8시 35분 민우
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
